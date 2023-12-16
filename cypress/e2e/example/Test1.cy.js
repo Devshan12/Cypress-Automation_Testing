31acbb3bf7a8c1e7f9d0511e3bf40810deef304a
Devshan <86223405+Devshan12@users.noreply.github.com>
2023-12-16 11:54:43 +0530

    css locaters with click a button

--- cypress/e2e/example/Test1.cy.js
+++ cypress/e2e/example/Test1.cy.js
@@ -1,6 +1,13 @@
+/// <reference types="Cypress" />
+
 describe('Test Suite', () => {
   it('Test Case1', () => {
       // navigate to indian web site
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+      cy.get('.search-keyword').type('ca')
+      cy.wait(2000)
+    //  cy.get('.product:visible').should('have.length',4)
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
   })
 })
