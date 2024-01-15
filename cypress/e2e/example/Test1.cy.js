95a2763a0820ddcbe78cd982033e0aad2d0d7819
Devshan <86223405+Devshan12@users.noreply.github.com>
2024-01-15 17:20:08 +0530

    check actual and expected results are correctly displaying

--- cypress/e2e/example/Test1.cy.js
+++ cypress/e2e/example/Test1.cy.js
@@ -1,35 +1,40 @@
 /// <reference types="Cypress" />
 
 // Import the EventEmitter module
-const EventEmitter = require('events');
+const EventEmitter = require('events')
 
 describe('Test Suite', () => {
   // Increase the maximum number of listeners for the "runner:resume" event
   beforeEach(() => {
-    EventEmitter.defaultMaxListeners = 20; // Set to a value higher than the current number of listeners
-  });
+    EventEmitter.defaultMaxListeners = 20 // Set to a value higher than the current number of listeners
+  })
 
   it('Test Case1', () => {
     // navigate to indian web site
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    cy.get('.search-keyword').type('ca');
-    cy.wait(2000);
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
 
-    cy.get('.products').as('productLocator');
-    cy.get('@productLocator').find('.product').should('have.length', 4);
-    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click();
-    console.log('sf')
+    cy.get('.products').as('productLocator')
+    cy.get('@productLocator').find('.product').should('have.length', 4)
+    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
+      console.log('sf')
+    })
+    
     // Add to cart item by its name
     cy.get('@productLocator').find('.product').each(($el) => {
-      const textName = $el.find('h4.product-name').text();
+      const textName = $el.find('h4.product-name').text()
       if (textName.includes('Cashews')) {
-        $el.find('button').click();
+        $el.find('button').click()
       }
-    });
+    })
+
+    // assert if logo text is correctly displayed
+    cy.get('.brand').should('have.text', 'GREENKART')
 
     // Take the logo title
-    cy.get('.brand').then(function (logoelement) {
-      cy.log(logoelement.text());
-    });
-  });
-});
+    cy.get('.brand').then(function (logoElement) {
+      cy.log(logoElement.text());
+    })
+  })
+})
