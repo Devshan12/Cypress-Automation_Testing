f5893d9a4a87454aa27e7fbc6decab599c2d98a2
Devshan <86223405+Devshan12@users.noreply.github.com>
2024-01-27 10:28:46 +0530

    Static drop down select

--- cypress/e2e/example/Test3.cy.js
+++ cypress/e2e/example/Test3.cy.js
@@ -15,7 +15,9 @@ describe('Test Suite', () => {
     cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/")
 
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
-    cy.get('#checkBoxOption1').uncheck()
+    cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+    cy.get('fieldset:contains("Checkbox Example")').find('input[type="checkbox"]').check(['option2','option3']).should('be.checked')
 
+    cy.get('select').select('option2').should('have.value','option2')
   })
 })
